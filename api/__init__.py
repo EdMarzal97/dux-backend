c572882a176a65ff08cd3b51c167c11eead72e4f
Edward S Marzal <edsanchezmarzal@gmail.com>
2023-08-08 18:55:43 -0500

    database connection

--- api/__init__.py
+++ api/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 # Creating a new "app" by using the Flask constructor. Passes __name__ as a parameter.
 app = Flask(__name__)
@@ -6,4 +7,4 @@ app = Flask(__name__)
 # Annotation that allows for the endpoints / URL to be hit.
 @app.route('/')
 def hello_world():
-	return 'Hello World!'
+	return 'Guten Morgen!'
--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from api import app
 # Checks to see if the name of the package is the run as the main package.
 if __name__ == "__main__":
 	# Runs the Flask application only if the main.py file is being run.
-	app.run()
+	app.run(debug=True)
