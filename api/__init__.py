9327c96fe7ca5a6132839260b7e3099460c87eb5
Edward S Marzal <edsanchezmarzal@gmail.com>
2023-08-08 19:00:13 -0500

    db connection

--- api/__init__.py
+++ api/__init__.py
@@ -1,8 +1,14 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 # Creating a new "app" by using the Flask constructor. Passes __name__ as a parameter.
 app = Flask(__name__)
+
+# Configuring a database for the Flask application defined above.
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{os.getcwd()}/dux.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
 
 # Annotation that allows for the endpoints / URL to be hit.
 @app.route('/')
