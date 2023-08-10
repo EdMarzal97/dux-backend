fb5f4eb52e479d709624db856a3f6c6a708da05b
Edward S Marzal <edsanchezmarzal@gmail.com>
2023-08-09 23:16:49 -0500

    imported resolvers they are still pending for bind

---
+++ api/resolvers.py
@@ -0,0 +1,102 @@
+from datetime import datetime
+from models.models import User, App, Lumos
+from api import db
+
+
+def resolve_users(obj, info):
+    return User.query.all()
+
+def resolve_user(obj, info, user_id):
+    return User.query.get(user_id)
+
+def resolve_create_user(obj, info, username, email, password):
+    user = User(username=username, email=email, password=password)
+    db.session.add(user)
+    db.session.commit()
+    return user
+
+def resolve_update_user(obj, info, user_id, input):
+    user = User.query.get(user_id)
+    if user:
+        for key, value in input.items():
+            setattr(user, key, value)
+        db.session.commit()
+        return user
+    return None
+
+def resolve_delete_user(obj, info, user_id, input):
+    user = User.query.get(user_id)
+    if user:
+        db.session.delete(user)
+        db.session.commit()
+        return True
+    return False
+        
+def resolve_apps(obj, info):
+    return App.query.all()
+
+def resolve_app(obj, info, app_id):
+    return App.query.get(app_id)
+
+def resolve_create_app(obj, info, app_name, app_icon):
+    app = App(app_name=app_name, app_icon=app_icon)
+    db.session.add(app)
+    db.session.commit()
+    return app
+
+def resolve_update_app(obj, info, app_id, input):
+    app = App.query.get(app_id)
+    if app:
+        for key, value in input.items():
+            setattr(app, key, value)
+        db.session.commit()
+        return app
+    return None
+
+def resolve_delete_app(obj, info, app_id):
+    app = App.query.get(app_id)
+    if app:
+        db.session.delete(app)
+        db.session.commit()
+        return True
+    return False
+
+def resolve_lumos(obj, info, lumos_id):
+    return Lumos.query.get(lumos_id)
+
+#possible resolvers for get Lumos information throught app or user
+#def resolve_lumos_for_user(obj, info, user_id):
+ #   return User.query.get(user_id).app_lumos
+
+#def resolve_lumos_for_app(obj, info, app_id):
+ #   return App.query.get(app_id).user_lumos
+
+def resolve_create_lumos(obj, info, user_id, app_id, permission_level):
+    lumos = Lumos(
+        user_id=user_id,
+        app_id=app_id,
+        permission_level=permission_level,
+        activation_date=datetime,
+        expiration_date=datetime,
+        account_status='active'
+    )
+    db.session.add(lumos)
+    db.session.commit()
+    return lumos
+
+def resolve_update_Lumos(obj, info, lumos_id, input):
+    lumos = Lumos.query.get(lumos_id)
+    if lumos:
+        for key, value in input.items():
+            setattr(lumos, key, value)
+        db.session.commit()
+        return lumos
+    return None
+
+def resolve_delete_Lumos(obj, info, lumos_id):
+    lumos = Lumos.query.get(lumos_id)
+    if lumos:
+        db.session.delete(lumos)
+        db.session.commit()
+        return True
+    return False
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 """main file for start the app"""
 from api import app, db
 from models import models
+from ariadne import make_executable_schema, load_schema_from_path, ObjectType
+from api.resolvers import resolve_users, resolve_user, resolve_create_user, resolve_update_user, resolve_delete_user, resolve_apps, resolve_app, resolve_create_app, resolve_update_app, resolve_delete_app, resolve_lumos, resolve_create_lumos, resolve_update_Lumos, resolve_delete_Lumos
+
 
 app.app_context().push()
 # Checks to see if the name of the package is the run as the main package.
